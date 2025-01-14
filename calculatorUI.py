e86f4bdd6b6cfca312fe6d88f12175021dcc0bba
shankar361 <40621440+shankar361@users.noreply.github.com>
2025-01-14 19:56:32 +0530

    Update calculatorUI.py

--- calculatorUI.py
+++ calculatorUI.py
@@ -9,5 +9,5 @@ y=sl.slider("Take second value",0,100,5)
 inputdataJson={"x":x,"y":y,"options":choice}
 if sl.button("Calculate"):
     sl.write("The Result is:")
-    res = requests.post(url="http://127.0.0.1:8000/calculate",data=json.dumps(inputdataJson))
+    res = requests.post(url="192.168.176.51:8001/calculate",data=json.dumps(inputdataJson))
     sl.subheader(res.text)
